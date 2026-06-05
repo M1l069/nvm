ff23c18ac30ad5857ff9078bb764e28963056b07
Miloslav Šmíd <miloslavsmid02@gmail.com>
2026-06-05 12:09:48 +0200

    pridaný nápis MENU k tlačidlu na zrolovanie menu v navbare

--- database/migrations/2026_05_24_193416_create_guardians_table.php
+++ database/migrations/2026_05_24_193416_create_guardians_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -29,3 +28,7 @@ return new class extends Migration
         Schema::dropIfExists('guardians');
     }
 };
+
+
+
+
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,8 +13,8 @@
                 <button
                     type="button"
                     @click="open = !open"
-                    class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-white"
-                >
+                    class="inline-flex items justify-center rounded-md p-2 text-gray-400 hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-white"
+                > <span class="pr-2">MENU</span>
                     <span class="sr-only">Otvoriť menu</span>
 
                     {{-- Hamburger ikona --}}
